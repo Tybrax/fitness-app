c96ee55a124ee75974417ddf776982aad85c4a4e
Bastien <bastien.ratat@gmail.com>
2020-03-17 16:23:03 +0100

    work on sessions, allow user to sign up, log in and log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  # posts and site
+  get 'sessions/new'
   root 'posts#index'
   get 'about/index'
   get 'site/home'
+  # routes for users
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :posts
   resources :users
